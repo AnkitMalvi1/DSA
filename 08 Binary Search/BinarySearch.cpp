3fc401beed178ab574ca8b1081206abe41fa3a56
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2023-07-21 19:41:05 +0530

    Add files via upload

---
+++ 08 Binary Search/BinarySearch.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    // int mid = (start + end) / 2;
+    int mid = start + (end - start) / 2;
+
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+            return mid;
+        if (key > arr[mid])
+            start = mid + 1;
+        else
+            end = mid - 1;
+        mid = (start + end) / 2;
+        mid = start + (end - start) / 2;
+    }
+    return -1;
+}
+
+int main()
+{
+    int even[6] = {2, 4, 6, 8, 12, 18};
+    int odd[5] = {3, 8, 9, 11, 16};
+    cout << "Key is found at Index of even[6] : " << binarySearch(even, 6, 12) << endl;
+    cout << "Key is found at Index of odd[5] : " << binarySearch(odd, 5, 11) << endl;
+
+    return 0;
+}
